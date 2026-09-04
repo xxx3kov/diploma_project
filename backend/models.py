de2219b4dcd967c4213a12135e78dfe556201888
xxx3kov <sanek.tretyakov565@gmail.com>
2026-09-04 22:20:57 +0300

    Add product and parameter models with constrains

--- backend/models.py
+++ backend/models.py
@@ -7,26 +7,116 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
 class Shop(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    url = models.URLField()
+    name = models.CharField(verbose_name="Наименование", max_length=50, unique=True)
+    url = models.URLField(verbose_name="Ссылка")
 
     class Meta:
-        verbose_name = 'Магазин'
+        verbose_name = "Магазин"
         verbose_name_plural = "Магазины"
-        ordering = ['name']
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
+
 class Category(models.Model):
-    shops = models.ManyToManyField(Shop, related_name='categories')
-    name = models.CharField(max_length=50, unique=True)
+    shops = models.ManyToManyField(
+        Shop, verbose_name="Магазины", related_name="categories"
+    )
+    name = models.CharField(verbose_name="Название", max_length=50, unique=True)
 
     class Meta:
-        verbose_name = 'Категория'
+        verbose_name = "Категория"
         verbose_name_plural = "Категории"
-        ordering = ['name']
+        ordering = ["name"]
 
     def __str__(self):
-        return self.name 
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        verbose_name="Категория",
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+    name = models.CharField(verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Список продуктов"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class ProductInfo(models.Model):
+    product = models.ForeignKey(
+        Product,
+        verbose_name="Продукт",
+        on_delete=models.CASCADE,
+        related_name="product_infos",
+    )
+    shop = models.ForeignKey(
+        Shop,
+        verbose_name="Магазин",
+        on_delete=models.CASCADE,
+        related_name="product_infos",
+    )
+    name = models.CharField(verbose_name="Имя")
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    price = models.DecimalField(
+        verbose_name="Стоимость", max_digits=10, decimal_places=2
+    )
+    price_rrc = models.DecimalField(
+        verbose_name="Розничная стоимость", max_digits=10, decimal_places=2
+    )
+
+    class Meta:
+        verbose_name = "Информация о продукте"
+        verbose_name_plural = "Информационный список продуктов"
+        ordering = ["name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["product", "shop", "name"], name="unique_product_info"
+            )
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Parameter(models.Model):
+    name = models.CharField(verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Имя параметра"
+        verbose_name_plural = "Список имён параметров"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class ProductParameter(models.Model):
+    product_info = models.ForeignKey(
+        ProductInfo,
+        verbose_name="Информация о продукте",
+        related_name="product_parameters",
+    )
+    parameter = models.ForeignKey(
+        Parameter, verbose_name="Параметр", related_name="product_parameters"
+    )
+    value = models.CharField(verbose_name="Значение", max_length=100)
+
+    class Meta:
+        verbose_name = "Параметр"
+        verbose_name_plural = "Список параметров"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.parameter.name}: {self.value}"
--- orders/settings.py
+++ orders/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mcf#o%^$d(%-w&l9qczw%djp=w%t4qgfr^wtc2qteobqsw#q^t'
+SECRET_KEY = "django-insecure-mcf#o%^$d(%-w&l9qczw%djp=w%t4qgfr^wtc2qteobqsw#q^t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,53 +31,53 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'backend',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "backend",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'orders.urls'
+ROOT_URLCONF = "orders.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'orders.wsgi.application'
+WSGI_APPLICATION = "orders.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -87,16 +87,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -116,14 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
 MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
+    "default": {
+        "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
 }
+
+AUTH_USER_MODEL = "backend.User"
