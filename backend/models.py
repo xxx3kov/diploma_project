2376ce3183898d595a1b5738eebb6eaa560ee0f3
xxx3kov <sanek.tretyakov565@gmail.com>
2026-09-04 21:36:33 +0300

    Add custom User, Shop and Category models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+class Shop(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    url = models.URLField()
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = "Магазины"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    shops = models.ManyToManyField(Shop, related_name='categories')
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name 
