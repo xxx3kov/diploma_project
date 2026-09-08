747697704c8de74f37357768b5e1a323eb8e7f56
xxx3kov <sanek.tretyakov565@gmail.com>
2026-09-09 00:39:03 +0300

    add path products

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from backend.views import LoginAccountView, PartnerUpdateView, RegisterAccountView
+from backend.views import LoginAccountView, PartnerUpdateView, ProductInfoView, RegisterAccountView
 
 urlpatterns = [
     path("partner/update", PartnerUpdateView.as_view(), name="partner-update"),
     path("user/register", RegisterAccountView.as_view(), name="user-register"),
     path("user/login", LoginAccountView.as_view(), name="user-login"),
+    path('products/', ProductInfoView.as_view(), name='product')
 ]
